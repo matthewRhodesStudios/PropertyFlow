c9e2bf34cf7afc125e0b597f495eaa70ba6b6ed7
unknown <matthewRhodesStudios@gmail.com>
2025-05-31 19:44:54 +0100

    Initial commit

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "drizzle-kit";
+import "dotenv/config";
 
 if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL, ensure the database is provisioned");
+  throw new Error("Missing DATABASE_URL. Check your .env file.");
 }
 
 export default defineConfig({
--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
@@ -59,11 +60,12 @@ app.use((req, res, next) => {
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
+  // was host: "0.0.0.0"  but that caused issues with vite
   const port = 5000;
   server.listen({
     port,
-    host: "0.0.0.0",
-    reusePort: true,
+    host: "127.0.0.1",
+    //reusePort: true,
   }, () => {
     log(`serving on port ${port}`);
   });
